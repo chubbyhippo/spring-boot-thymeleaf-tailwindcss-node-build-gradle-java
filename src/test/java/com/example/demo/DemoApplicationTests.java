938959a2a8e016db9ac2995e2fe83e1372dde64e
ch <mkmsacc@outlook.com>
2023-12-17 19:07:24 +0700

    Replace contextLoads test with main method test
    The old contextLoads test was removed and replaced with a more specific test that checks if the main method can run without throwing exceptions. A static import for assertThatNoException was also added to support the change.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void shouldRunMainWithoutException() {
+        var randomPortNumber = "--server.port=0";
+        assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{randomPortNumber}));
+    }
 
 }
